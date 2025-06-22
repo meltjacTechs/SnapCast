09e45d03d748e80f201e0e2b50035c408dde3dfc
meltjacTechs <ebwire02@gmail.com>
2025-06-22 19:59:22 +0300

    feat: add VideoCard component and integrate it into Page for displaying video details

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,26 @@
 import Header from "@/components/Header";
-
+import VideoCard from "@/components/VideoCard";
 
 const Page = () => {
-  return (
-    <main className="wrapper page">
-        <Header title="All videos" subHeader="Public Library"/>
+    return (
+        <main className="wrapper page">
+            <Header title="All videos" subHeader="Public Library" />
 
-       <h1 className="text-2xl font-karlaclear">Welcome to SnapCast</h1>
-    </main>
-  )
-}
+            <h1 className="text-2xl font-karlaclear">Welcome to SnapCast</h1>
 
-export default Page
+            <VideoCard
+                id="1"
+                title="Snap Chat Message - 30 June 2025"
+                thumbnail="/assets/samples/thumbnail (1).png"
+                createdAt={new Date("2025-05-01")} // ✅ Corrected this line
+                userImg="/assets/images/jason.png"
+                username="jason"
+                views={10}
+                visibility="public"
+                duration={157}
+            />
+        </main>
+    );
+};
+
+export default Page;
--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,6 +1,38 @@
-const VideoCard = () => {
+import Link from "next/link";
+import Image from "next/image";
+const VideoCard = ({
+                       id,
+                       title,
+                       thumbnail,
+                       userImg,
+                       username,
+                       createdAt,
+                       views,
+                       visibility,
+                       duration,
+                   }: VideoCardProps) => {
     return (
-        <div>VideoCard</div>
-    )
-}
-export default VideoCard
+        <Link href={`/video/${id}`} className="video-card">
+            <Image src={thumbnail} alt="thumbnail" width={290} height={160} className="thumbnail" />
+            <article>
+                <div>
+                    <figure>
+                        <Image src={userImg} alt="avatar" width={34} height={34} className="rounded-full aspect-square" />
+                        <figcaption>
+                            <h3>{username}</h3>
+                            <p>{visibility}</p>
+                        </figcaption>
+                    </figure>
+                    <aside>
+                        <Image src="/assets/icons/eye.svg" alt="views" width={16} height={16}/>
+
+                        <span>{views}</span>
+                    </aside>
+                </div>
+                <h2>{title} - {""}</h2>
+            </article>
+        </Link>
+    );
+};
+
+export default VideoCard;
