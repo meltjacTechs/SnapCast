25cd0ec90dd321c7bfccdc1661e0c9a46daa99af
meltjacTechs <ebwire02@gmail.com>
2025-06-22 16:12:03 +0300

    feat: update Header component to use dynamic user image and enhance page layout

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,8 +1,13 @@
+import Header from "@/components/Header";
 
 const Page = async ({ params } : ParamsWithSearch) => {
     const { id } = await params;
     return (
-        <div className="text-2xl font-karla">User Id: {id}</div>
+        <div className="wrapper page">
+            <Header subHeader="pro_grammer@meltjac.tech" title="Meltjac | Techs" userImg="/assets/images/dummy.jpg"/>
+
+            <h1 className="text-2xl font-karla" >User Id: {id}</h1>
+            </div>
     )
 }
 export default Page
