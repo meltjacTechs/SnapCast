b5ac315cd10603eb0962bd789a4197f8c536603c
meltjacTechs <ebwire02@gmail.com>
2025-06-22 14:12:02 +0300

    feat: add layout and navbar components for page structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-karlaclear">Welcome to SnapCast</h1>
+    </div>
+  )
+}
+
+export default Page
