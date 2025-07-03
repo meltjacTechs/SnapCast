383050ff084a1c48bcaa0e412f781eea39037917
meltjacTechs <ebwire02@gmail.com>
2025-07-04 00:57:53 +0300

    feat: update upload page to include FormField and FileInput components

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -1,7 +1,13 @@
+import FormField from "@/components/FormField";
+import FileInput from "@/components/FileInput";
 
 const Page = () => {
     return (
-        <div>Page</div>
+        <div className="wrapper-md upload-page">
+            <h1>Upload a video</h1>
+            <FormField />
+            <FileInput />
+        </div>
     )
 }
 export default Page
