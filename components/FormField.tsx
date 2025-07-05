2bc8aa0506435615f0645687dcc16c00fd5d3aa7
meltjacTechs <ebwire02@gmail.com>
2025-07-05 16:15:32 +0300

    feat: implement dynamic rendering for FormField component

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const FormField = () => {
-    return (
-        <div>FormField</div>
-    )
+const FormField = ({ id, label, type = "text", value, onChange, placeholder, as = "input", options = []}: FormFieldProps) => {
+
+ const InputToRender = ({type}: {type: string}) => {
+  if(type==='textarea') {
+      return <textarea />
+  } else if(type === 'select') {
+      return <select />
+  }else {
+      return <input />
+  }
+ }
+
+ return (
+    <div className="form-field">
+       <label htmlFor={id}>{label}</label>
+
+        <InputToRender type={as} />
+    </div>
+)
 }
 export default FormField
